b6f6c0e4efea6e1e4d0e7fe42a24d671054a5872
denise7792 <59653034+denise7792@users.noreply.github.com>
2020-01-21 17:30:29 +0100

    file course

---
+++ src/main/java/net/bitsrl/academia/database/SystemRepositoryCourse.java
@@ -0,0 +1,16 @@
+package net.bitsrl.academia.database;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class SystemRepositoryCourse {
+    File f = new File(
+            getClass().getClassLoader().getResource("systemRepositoryCourse").getFile()
+    );
+
+    Scanner s = new Scanner(new File("/java/resources/systemRepositoryCourse"));
+            )
+
+
+
+}
